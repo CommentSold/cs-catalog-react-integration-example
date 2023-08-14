7060a1b95f4b60ac90bf37b494cd2671225d1b8d
seth white <hyrum.s.white@gmail.com>
2023-08-14 11:27:38 -0400

    fix: added link to cs catalog

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,9 @@ function App() {
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
 			</div>
-			<h1>Vite + React</h1>
+			<a style={{ fontSize: "32px" }} href="/path/to/cs-catalog">
+				Go to CS Catalog
+			</a>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
